0aff2886070f3c4eccfa6514aac852c8b50b8d18
Kiana Long <kianajade42.kl@gmail.com>
2021-05-04 16:38:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+get'/' do
+  erb :super_hero
+end
+
+ post'/views/teams' do
+   @team = Team.new(params[:teams])
+   params[:teams][:members].each do |details|
+     Super.Hero.new(details)
+   end
+   @members = Super_Hero.all
+    erb :team
+ end
 
 end
